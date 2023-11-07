0e054c2c3044ce24f76e23084654849f030f8770
Asif Munshi <75932258+asif-munshi@users.noreply.github.com>
2023-11-08 01:24:22 +0600

    create an ErrorMessage component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { z } from 'zod'
 import { createIssueSchema } from '@/lib/validationSchemas'
+import ErrorMessage from '@/components/ErrorMessage/ErrorMessage'
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -48,11 +49,7 @@ export default function NewIssuePage() {
             <TextField.Root>
               <TextField.Input placeholder="Title" {...register('title')} />
             </TextField.Root>
-            {errors.title && (
-              <Text color="red" as="p">
-                {errors.title.message}
-              </Text>
-            )}
+            <ErrorMessage>{errors.title?.message}</ErrorMessage>
             <Controller
               name="description"
               control={control}
@@ -60,11 +57,7 @@ export default function NewIssuePage() {
                 <SimpleMDE placeholder="Description" {...field} />
               )}
             />
-            {errors.description && (
-              <Text color="red" as="p">
-                {errors.description.message}
-              </Text>
-            )}
+            <ErrorMessage>{errors.description?.message}</ErrorMessage>
             <Button className="hover:cursor-pointer">Add Issue</Button>
           </div>
         </form>
---
+++ components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,12 @@
+import { Text } from '@radix-ui/themes'
+import { PropsWithChildren } from 'react'
+
+export default function ErrorMessage({ children }: PropsWithChildren) {
+  if (!children) return null
+
+  return (
+    <Text color="red" as="p">
+      {children}
+    </Text>
+  )
+}
