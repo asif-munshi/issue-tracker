667bc661b0150387c78f1d5093a1b7a7908dd8d3
Asif Munshi <75932258+asif-munshi@users.noreply.github.com>
2023-11-08 00:58:59 +0600

    Handle create issue errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,9 +3,10 @@
 import SimpleMDE from 'react-simplemde-editor'
 import 'easymde/dist/easymde.min.css'
 import axios from 'axios'
-import { Button, TextField } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import { useForm, Controller } from 'react-hook-form'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 interface IssueForm {
   title: string
@@ -15,29 +16,42 @@ interface IssueForm {
 export default function NewIssuePage() {
   const router = useRouter()
   const { control, register, handleSubmit } = useForm<IssueForm>()
+  const [error, setError] = useState('')
+
   return (
-    <>
-      <form
-        className="flex w-full justify-center"
-        onSubmit={handleSubmit(async (data) => {
-          await axios.post('/api/issues', data)
-          router.push('/issues')
-        })}
-      >
-        <div className="flex w-[576px] flex-col gap-5">
-          <TextField.Root>
-            <TextField.Input placeholder="Title" {...register('title')} />
-          </TextField.Root>
-          <Controller
-            name="description"
-            control={control}
-            render={({ field }) => (
-              <SimpleMDE placeholder="Description" {...field} />
-            )}
-          />
-          <Button>Add Issue</Button>
-        </div>
-      </form>
-    </>
+    <div className="flex w-full justify-center">
+      <div className="w-[576px]">
+        {error && (
+          <Callout.Root color="red" className="mb-5">
+            <Callout.Text>{error}</Callout.Text>
+          </Callout.Root>
+        )}
+        <form
+          className="flex w-full justify-center"
+          onSubmit={handleSubmit(async (data) => {
+            try {
+              await axios.post('/api/issues', data)
+              router.push('/issues')
+            } catch (error) {
+              setError('An unexpected error occured.')
+            }
+          })}
+        >
+          <div className="flex w-full flex-col gap-5">
+            <TextField.Root>
+              <TextField.Input placeholder="Title" {...register('title')} />
+            </TextField.Root>
+            <Controller
+              name="description"
+              control={control}
+              render={({ field }) => (
+                <SimpleMDE placeholder="Description" {...field} />
+              )}
+            />
+            <Button>Add Issue</Button>
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
